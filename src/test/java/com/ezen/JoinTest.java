23cb186ce1a55c6a0209993932a5f4e821dc3e89
Donghwi <kwonhwi155@naver.com>
2022-07-18 15:00:46 +0900

    장바구니 구현 완료

---
+++ src/test/java/com/ezen/JoinTest.java
@@ -0,0 +1,15 @@
+package com.ezen;
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JoinTest {
+
+	public JoinTest() {
+		System.out.println("feature/1 브랜치를 위한 테스트");
+		System.out.println("issue#1 장바구니 구현 완료");
+	}
+}
